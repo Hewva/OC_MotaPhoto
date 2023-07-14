c9aca328340ee18623e0270b5fae592cb7f39a4c
Hawa SYLLA <hawasylla51@gmail.com>
2023-07-14 14:48:52 +0200

    menu footer

--- app/public/wp-content/themes/MotaPhoto/footer.php
+++ app/public/wp-content/themes/MotaPhoto/footer.php
@@ -1,4 +1,5 @@
 <footer id="site-footer" class="header-footer-group"> 
+    <!-- Pour afficher le secondary menu crée dans WP -->
 <?php wp_nav_menu(array('theme_location' => 'footer'));?>
         </footer><!-- #site-footer -->
 
