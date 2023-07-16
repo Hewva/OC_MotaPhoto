9ec6e266a29b9571c2ac2843a3493423000da803
Hawa SYLLA <hawasylla51@gmail.com>
2023-07-16 22:38:38 +0200

    appeler modale dans footer

--- app/public/wp-content/themes/MotaPhoto/footer.php
+++ app/public/wp-content/themes/MotaPhoto/footer.php
@@ -2,7 +2,8 @@
     <!-- Pour afficher le secondary menu crée dans WP -->
 <?php wp_nav_menu(array('theme_location' => 'footer'));?>
         </footer><!-- #site-footer -->
-
+    <!-- Pour afficher le modale-contact -->
+    <?php get_template_part( 'templates_part/modale-contact' ); ?>
       <?php wp_footer(); ?>
 
    </body>
