7ab95e42e2b882b7f2c73f0408bdc997c1ba92bc
Hawa SYLLA <hawasylla51@gmail.com>
2023-07-20 19:50:10 +0200

    MAJ section header

--- app/public/wp-content/themes/MotaPhoto/header.php
+++ app/public/wp-content/themes/MotaPhoto/header.php
@@ -1,17 +1,3 @@
-<?php
-/**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
-?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
 <head>
@@ -23,24 +9,15 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<header id="site-header" class="header-footer-group sticky-top">
-<div class="header-inner section-inner">
-
-<div class="header-titles-wrapper">
-	<a class="skip-link screen-reader-text" href="#content">
-		<?php
-		/* translators: Hidden accessibility text. */
-		esc_html_e( 'Skip to content', 'twentytwentyone' );
-		?>
-	</a>
-
-	<div id="site-logo">
+<header id="site-header" class = "site-header-container">
+<div id="site-logo" class = "site-logo-container">
     <?php if (has_custom_logo()) {
         the_custom_logo();
-    } else {
-        echo '<a href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a>';
     } ?>
 </div>
+
 <nav id="site-navigation" class="main-navigation">
+
     <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 </nav>
+</header>
