8168879c1096c524bbb6d5fa610874b19697fedb
Hawa SYLLA <hawasylla51@gmail.com>
2023-07-21 04:05:21 +0200

    MAJ modale de contact

---
+++ app/public/wp-content/themes/MotaPhoto/templates_part/modale-contact.php
@@ -0,0 +1,10 @@
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-container">
+    <img src="<?php echo get_stylesheet_directory_uri() ; ?>/images/Contact-header.png" />
+    <?php echo do_shortcode('[contact-form-7 id="69" title="modale_contact"]'); ?>
+</div>
+
+</div>
