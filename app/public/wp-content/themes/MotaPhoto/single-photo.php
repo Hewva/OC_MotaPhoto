75ca55d25e371a33894413acbbe75edff447c7fa
Hawa SYLLA <hawasylla51@gmail.com>
2023-08-02 21:44:56 +0200

    MAJ template

--- app/public/wp-content/themes/MotaPhoto/single-photo.php
+++ app/public/wp-content/themes/MotaPhoto/single-photo.php
@@ -12,34 +12,25 @@ get_header(); ?>
         <?php
         // Start the loop.
         while ( have_posts() ) : the_post(); ?>
-        <div class="photo-content">
         <div class="photo-container">
-        <div class="photo-filters">
-
-        </div>
-            <h2>
-                <?php the_title() ?>
-            </h2>
-<div class="field">
-    <p class = "photo-field">"Référence":
-    <span id="photo-ref">
-        <?php get_field('reference'); ?></span>
-     </p>
-    <p class = "photo-field">"Catégorie":
-    <?php echo get_the_terms(get_the_ID(), 'categorie'); ?>
-     </p>
-     <p class = "photo-field">"Type":
-    <?php echo get_the_terms(get_the_ID(), 'type'); ?>
-     </p>
-     <p class = "photo-field">"Format":
-    <?php echo get_the_terms(get_the_ID(), 'format'); ?>
-     </p>
-     <p class = "photo-field">"Année":
-    <?php echo date('Y'); ?>
-     </p>
+<!-- Bloc 50% de largeur à gauche -->
+<div class="photo-info-left">     
+    <h2><?php the_title() ?></h2>      
+    <p>"RÉFÉRENCE":<?php get_field('reference'); ?></p>
+    <p>"CATÉGORIE":<?php echo get_the_terms(get_the_ID(), 'categorie'); ?></p>
+    <p>"FORMAT":<?php echo get_the_terms(get_the_ID(), 'format'); ?></p>
+    <p>"TYPE":<?php echo get_the_terms(get_the_ID(), 'type'); ?></p>
+    <p>"ANNÉE":<?php echo date('Y'); ?></p>
 </div>
-<div class="photo-catalog">
-    <?php the_post_thumbnail(); ?>
+<!-- Bloc 50% de largeur à droite -->
+<div class="photo-info-right">
+    <div class= "photo-img">    
+    <?php the_post_thumbnail('full'); ?>
+</div>
+<!-- En dessous des infos de droite -->
+<div class ="photo-info-behind">
+    <p>Cette pohoto vous intéresse ?</p>
+    <button id="contact-button" class="modal-contact-link" data-photo-ref="<?php echo get_field('reference'); ?>
 </div>
         <?php  get_template_part( 'content', get_post_format() );
  
