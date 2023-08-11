bec33dcf4b732d5c97b383d5e8e741563a00782a
Hawa SYLLA <hawasylla51@gmail.com>
2023-08-11 13:46:06 +0200

    MAJ template

--- app/public/wp-content/themes/MotaPhoto/single-photo.php
+++ app/public/wp-content/themes/MotaPhoto/single-photo.php
@@ -12,41 +12,43 @@
  // Start the loop.
  while ( have_posts() ) : the_post(); ?>
  <div class="photo-container">
-     <!-- Bloc 50% de largeur à gauche -->
+     <!-- Bloc de gauche -->
      <div class="photo-info-left">     
          <h2><?php the_title(); ?></h2>
          <p>RÉFÉRENCE: <?php echo get_field('reference'); ?></p>
          <p>CATÉGORIE : <?php echo get_the_term_list(get_the_ID(), 'categ', '', ', '); ?></p>
          <p>FORMAT : <?php echo get_the_term_list(get_the_ID(), 'format', '', ', '); ?></p>
          <p>TYPE: <?php echo get_field('type'); ?></p>
-         <p>ANNÉE: <?php echo date('Y'); ?></p>
+         <p>ANNÉE: <?php echo get_the_date('Y'); ?></p>
+         <p>Cette photo vous intéresse ?</p>
+         <button id="contact-button" class="modal-contact-link open-modal-contact" data-photo-ref="<?php echo get_field('reference'); ?>">Contact</button>
      </div>
-     <!-- Bloc 50% de largeur à droite -->
+     <!-- Bloc de droite -->
      <div class="photo-info-right">
          <div class="photo-img">    
              <?php the_post_thumbnail('full'); ?>
          </div>
-         <!-- Bloc 118px en dessous à gauche (texte et bouton contact) -->
-         <div class="photo-info-behind">
-             <p>Cette photo vous intéresse ?</p>
-             <button id="contact-button" class="modal-contact-link" data-photo-ref="<?php echo get_field('reference'); ?>">Contact</button>
-         </div>
-         <!-- Bloc 118px en dessous à droite (miniatures et flèches de navigation) -->
-         <div class="photo-links">
-             <?php
-             $prev_post = get_previous_post();
-             $next_post = get_next_post();
-             ?>
-             <?php if (!empty($prev_post)) : ?>
-                 <a href="<?php echo get_permalink($prev_post->ID); ?>" class="prev-link">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/arrowL.png" alt="Précédent"/></a>
-             <?php endif; ?>
-             <?php if (!empty($next_post)) : ?>
-                 <a href="<?php echo get_permalink($next_post->ID); ?>" class="next-link">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/arrowR.png" alt="Suivant"/></a>
-             <?php endif; ?>
-         </div>
      </div>
+ </div>
+ 
+ <!-- Miniature et liens de navigation -->
+ <div class="photo-navigation">
+     <?php
+     $prev_post = get_previous_post();
+     $next_post = get_next_post();
+     ?>
+     <?php if (!empty($prev_post)) : ?>
+         <a href="<?php echo get_permalink($prev_post->ID); ?>" class="prev-link">
+             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/arrowL.png" alt="Précédent"/>
+                <?php echo get_the_post_thumbnail($prev_post->ID, 'thumbnail', array('class' => 'thumbnail')); ?>
+         </a>
+     <?php endif; ?>
+     <?php if (!empty($next_post)) : ?>
+         <a href="<?php echo get_permalink($next_post->ID); ?>" class="next-link">
+             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/arrowR.png" alt="Suivant"/>
+             <?php echo get_the_post_thumbnail($next_post->ID, 'thumbnail', array('class' => 'thumbnail')); ?>
+         </a>
+     <?php endif; ?>
  </div>
  
  <?php endwhile;
