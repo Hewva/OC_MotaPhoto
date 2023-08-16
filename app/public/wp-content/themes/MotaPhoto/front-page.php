0410f643d2cbd9dd2eb19b87bcc9a39613443f79
Hawa SYLLA <hawasylla51@gmail.com>
2023-08-16 06:16:18 +0200

    MAJ Hero

--- app/public/wp-content/themes/MotaPhoto/front-page.php
+++ app/public/wp-content/themes/MotaPhoto/front-page.php
@@ -1,25 +1,29 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+<?php 
+/*
+ * The template for the Home page
  */
+?>
 
-get_header();
+<?php get_header(); ?>
 
-/* Start the Loop */
-while ( have_posts() ) :
-	the_post();
-	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-endwhile; // End of the loop.
-
-get_footer();
+<?php 
+$query = new WP_Query(
+	$args = array(
+		'post_type' => 'photo',
+		'posts_per_page' => 1,
+		'orderby' => 'rand',
+	)
+); 
+while ($query->have_posts()) : $query->the_post(); ?>
+	<div class= "hero-home">
+	<div class= "hero-photo">
+	<?php the_post_thumbnail('full'); ?>
+	</div>
+	<div class= "hero-title">
+		<h1>PHOTOGRAPHE EVENT</h1>
+	</div>
+	<? wp_reset_postdata(); ?>
+	<?php 
+endwhile; ?> 
+</div>
+<?php get_footer(); ?>
